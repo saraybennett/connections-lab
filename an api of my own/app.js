1e0df5b93fba83d96e093e9186cb2b9a7fc411e9
Sara <78770797+saraybennett@users.noreply.github.com>
2025-09-27 12:31:55 -0400

    Add files via upload

--- an api of my own/app.js
+++ an api of my own/app.js
@@ -1,66 +1,160 @@
-window.addEventListener('load', () => {
-    console.log('page is loaded')
-});
+window.addEventListener("load", () => {
+  //1. fetch data from api on page load
+  fetch("fandom.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // 2. display that data on the webpage
+      randomNumber = Math.floor(Math.random() * data.length);
+      randomNumberTwo = Math.floor(Math.random() * data.length);
 
-//listen for button click event
-let button = document.getElementById('button');
+      //checking that everything is working using console.log(data[randomNumber].name);
 
-//1. listen to event click on the button 2. fetch data from api 3. display that data on the webpage
-button.addEventListener('click', function () {
-    fetch('https://www.freepublicapis.com/api/random')
-        .then(response => response.json())
-        .then(data => {
-            
-            let nameElement = document.querySelector('#api-name');
-            nameElement.innerHTML = data.title;
+      //first fandom data
+      let fandomButtonOne = document.querySelector("#fandom_name");
+      let fandomNameOne = data[randomNumber].name;
+      fandomButtonOne.innerHTML = fandomNameOne;
 
-            let documentationElement = document.querySelector('#api-documentation');
-            documentationElement.innerHTML = data.documentation;
+      let numWorksOne = data[randomNumber].cached_count;
+      console.log(numWorksOne);
 
-            let descriptionElement = document.querySelector('#api-description');
-            descriptionElement.innerHTML = data.description;
+      //second fandom data
+      let fandomButtonTwo = document.querySelector("#fandom_name_two");
+      let fandomNameTwo = data[randomNumberTwo].name;
+      fandomButtonTwo.innerHTML = fandomNameTwo;
+
+      let numWorksTwo = data[randomNumberTwo].cached_count;
+      console.log(numWorksTwo);
+
+      // let numElement = document.querySelector('#fandom_num');
+      // numElement.innerHTML = data[randomNumber].cached_count;
+
+      //figure out which fandom has more works - do I need this statement?
+      let largerFandom;
+
+      if (numWorksTwo > numWorksOne) {
+        largerFandom = fandomNameTwo;
+      } else {
+        largerFandom = fandomNameOne;
+      }
+
+      console.log(largerFandom);
+
+      //listen for the button click on correct fandom
+      // let button = document.getElementById('button');
+      //detect click event for each button , in each button, ask if numworks if bigger than the other num works and then perform something based on that
+      //in the click event, determine if they clicked the right button
+
+      //when user clicks fandom button one, something happens depending on if that fandom is bigger
+      fandomButtonOne.addEventListener("click", function () {
+        //for some reason could not get this to work properly
+        //     if (largerFandom = fandomNameOne) {
+        //  console.log("number one bigger");
+        //  window.confirm("Correct!");
+
+        //     }
+
+        //     else {
+        //         console.log("number two bigger");
+        //         window.confirm("Sorry, better luck next time :/");
+        //     }
 
 
-            // let descriptionElement = document.querySelector('#api-description');
-            // descriptionElement.innerHTML = data.description;
-        })
-        .catch(function (err) {
-            console.error(err);
+
+          if (numWorksOne > numWorksTwo) {
+            console.log("number one bigger");
+            window.confirm(
+              "Correct! " +
+                fandomNameOne +
+                " has " +
+                numWorksOne +
+                " works on the Archive! " +
+                fandomNameTwo +
+                " has " +
+                numWorksTwo +
+                " works."
+            );
+
+            location.reload();
+          } else {
+            console.log("number two bigger");
+            window.confirm(
+              "Sorry, better luck next time :/ " +
+                fandomNameTwo +
+                " has " +
+                numWorksTwo +
+                " works on the Archive and " +
+                fandomNameOne +
+                " has " +
+                numWorksOne +
+                " works."
+            );
+            location.reload();
+          }
         });
+
+      //when user clicks fandom button two, something happens depending on if that fandom is bigger
+      fandomButtonTwo.addEventListener("click", function () {
+        if (numWorksTwo > numWorksOne) {
+          console.log("number two bigger");
+          window.confirm(
+            "Correct! " +
+              fandomNameTwo +
+              " has " +
+              numWorksTwo +
+              " works on the Archive! " +
+              fandomNameOne +
+              " has " +
+              numWorksOne +
+              " works."
+          );
+          location.reload();
+        } else {
+          console.log("number one bigger");
+          window.confirm(
+            "Sorry, better luck next time :/ " +
+              fandomNameOne +
+              " has " +
+              numWorksOne +
+              " works on the Archive and " +
+              fandomNameTwo +
+              " has " +
+              numWorksTwo +
+              " works."
+          );
+          location.reload();
+        }
+      });
+
+      //figure out if they selected the fandom with more works
+      //if they selected right fandom, randomize the other one
+      //if they didn't, it ends - sad end screen
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
 });
 
-//fetch random api : https://www.freepublicapis.com/api/random
+// //listen for button click event
+// let button = document.getElementById('button');
 
-//Load the json data file
-//    fetch('https://www.freepublicapis.com/api/random')
-//    .then(response => response.json())
-//    .then(data => {
-//         console.log(data.title);
-//         //Do something with 'data'
-
-//    })
-//    .catch(error => {
-//        console.log("Error!!! : " + error);
-//    })
-
-
-
+// //1. listen to event click on the button 2. fetch data from api 3. display that data on the webpage
 // button.addEventListener('click', function () {
-//     fetch('https://whicheverapi.com/path/id')
-//         .then(function (result) {
-//             console.log(result);
+//     fetch('https://www.freepublicapis.com/api/random')
+//         .then(response => response.json())
+//         .then(data => {
+
+//             let nameElement = document.querySelector('#api-name');
+//             nameElement.innerHTML = data.title;
+
+//             let documentationElement = document.querySelector('#api-documentation');
+//             documentationElement.innerHTML = data.documentation;
+
+//             let descriptionElement = document.querySelector('#api-description');
+//             descriptionElement.innerHTML = data.description;
+
+//             // let descriptionElement = document.querySelector('#api-description');
+//             // descriptionElement.innerHTML = data.description;
 //         })
 //         .catch(function (err) {
 //             console.error(err);
-//         });
-// });
-
-//show the constraint page
-// function foobar(){
-//     x=document.getElementById("wake_up");
-//     x.classList.add("hide");
-//     y=document.getElementById("mirror_look");
-//     y.classList.add("hide");
-//     z=document.getElementById("mirror_image");
-//     z.classList.remove("hide");
-// }
+//         })
